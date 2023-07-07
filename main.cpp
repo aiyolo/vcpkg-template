58a11d34b4f056210e74138669aa63f87bbc9302
aiyolo <cretaboy001@gmail.com>
2023-07-07 12:13:21 +0800

    vcpkg version management

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include "mainwindow.h"
+#include <fmt/core.h>
+#include <zlib.h>
+#include <iostream>
+#include <boost/version.hpp>
 
-#include <QApplication>
-#include <QDebug>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    qDebug() << "hello world!";
-    return a.exec();
+int main() {
+  fmt::print("fmt version is {}\n"
+             "zlib version is {}\n",
+             FMT_VERSION, ZLIB_VERSION);
+  std::cout << "Hellow World\n";
+  std::cout << "Boost version is " << BOOST_LIB_VERSION << std::endl;
+  return 0;
 }
